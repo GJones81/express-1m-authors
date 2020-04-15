edf9acac4b74f4371818489cf553ba994a7afa32
gjones81 <jones.guy81@gmail.com>
2020-04-15 15:12:47 -0700

    Updating

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,20 +1,50 @@
 let router = require('express').Router()
+//require database models
+let db = require('../models')
 
+//go to the database a findAll
 router.get('/', (req, res) => {
-  res.render('authors/index')
+  db.author.findAll()
+  	.then((authors) => {
+  	res.render('authors/index', {authors})
+  	})
+  	.catch((err) => {
+  	console.log('Error', err)
+  	res.render('error')
+  })		
 })
 
+//create a new author
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('posted to authors')
+	db.author.create(req.body)
+	.then(() => {
+		res.redirect('/authors')
+	})
+	.catch((err) => {
+		console.log('Error', err)
+		res.render('error')
+	})
 })
 
 router.get('/new', (req, res) => {
   res.render('authors/new')
 })
 
+//shows the author selected from the Authors page by id
 router.get('/:id', (req, res) => {
-  res.render('authors/show')
+	db.author.findOne({
+		where:{ id: req.params.id },
+		//joins to the book table, includes the related data 
+		include: [ db.book ]
+	})
+	.then((author) => {
+		res.render('authors/show', { author })
+	})
+	.catch((err) => {
+		console.log('Error', err)
+		res.render('error')
+	})
+  
 })
 
 module.exports = router
